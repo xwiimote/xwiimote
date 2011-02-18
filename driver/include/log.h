786089b4a2f057bec7cf3cf0f20fc1c6760d5a61
David Herrmann <dh.herrmann@gmail.com>
2011-02-18 16:05:14 +0100

    Add log interface
    Add log module which logs into system log files or temporary log
    files stored in /tmp or /var/log.

---
+++ driver/include/log.h
@@ -0,0 +1,42 @@
+/*
+ * XWiimote - driver - log.h
+ * Written by David Herrmann, 2010, 2011
+ * Dedicated to the Public Domain
+ */
+
+/*
+ * Log Interface
+ * The log interface opens a logfile when created, prints the
+ * messages with additional information like time to the file
+ * and optionally removes the logfile if destroyed.
+ * Every logfile has a unique name and is owned by this process.
+ *
+ * Namespace: WII_LOG_*
+ *            wii_log_*
+ *
+ * w_log_open() opens a new logfile or returns false on failure.
+ * w_log_do() and w_log_vdo() print a message to an open logfile
+ * and return false on failure. Log failures should be ignored
+ * by the application because there is no nice way to recover.
+ * w_log_close() frees all allocated memory.
+ */
+
+#ifndef WII_LOG_H
+#define WII_LOG_H
+
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+struct wii_log {
+	signed int fd;
+	char path[100];
+	bool tmp;
+};
+
+extern bool wii_log_open(struct wii_log *logger, const char *name, bool tmp);
+extern void wii_log_close(struct wii_log *logger);
+extern bool wii_log_do(struct wii_log *logger, const char *format, ...);
+extern bool wii_log_vdo(struct wii_log *logger, const char *format, va_list list);
+
+#endif /* WII_LOG_H */
---
+++ driver/src/log.c
@@ -0,0 +1,71 @@
+/*
+ * XWiimote - driver - log.c
+ * Written by David Herrmann, 2010, 2011
+ * Dedicated to the Public Domain
+ */
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "log.h"
+
+static signed int open_log(char *path, size_t len, const char *name, bool tmp)
+{
+	signed int fd;
+	unsigned int i;
+
+	if (tmp) {
+		for (i = 0; i < 100; ++i) {
+			snprintf(path, len, "/tmp/xwiimote-%s.%d.log", name, rand());
+			fd = open(path, O_WRONLY | O_CREAT | O_EXCL | O_APPEND, S_IRUSR | S_IWUSR);
+			if (fd >= 0)
+				return fd;
+		}
+		return -1;
+	}
+	else {
+		snprintf(path, len, "/var/log/xwiimote-%s.log", name);
+		return open(path, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+	}
+}
+
+bool wii_log_open(struct wii_log *logger, const char *name, bool tmp)
+{
+	logger->fd = open_log(logger->path, sizeof(logger->path), name, tmp);
+	if (logger->fd < 0)
+		return false;
+
+	logger->tmp = tmp;
+	return logger;
+}
+
+void wii_log_close(struct wii_log *logger)
+{
+	close(logger->fd);
+	if (logger->tmp)
+		unlink(logger->path);
+}
+
+bool wii_log_do(struct wii_log *logger, const char *format, ...)
+{
+	va_list list;
+	bool ret;
+
+	va_start(list, format);
+	ret = wii_log_vdo(logger, format, list);
+	va_end(list);
+	return ret;
+}
+
+bool wii_log_vdo(struct wii_log *logger, const char *format, va_list list)
+{
+	if (vdprintf(logger->fd, format, list) < 1)
+		return false;
+	else
+		return true;
+}
