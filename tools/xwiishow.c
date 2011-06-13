d938541b77fd442a244919e793b92ef287470a62
David Herrmann <dh.herrmann@googlemail.com>
2011-06-13 15:42:23 +0200

    Add xwiishow dummy
    xwiishow will, when ready, visualize the current state of a connected wiimote
    and can be used for debugging.

---
+++ tools/xwiishow.c
@@ -0,0 +1,21 @@
+/*
+ * XWiimote - tools
+ * Written 2010, 2011 by David Herrmann
+ * Dedicated to the Public Domain
+ */
+
+/*
+ * XWiimote Visualizer
+ * This tool selects a connected wiimote and prints its current state to the
+ * screen. It listens to new events and updates the state accordingly.
+ * Exit with ctrl+c.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <xwiimote.h>
+
+int main(int argc, char **argv)
+{
+	return EXIT_SUCCESS;
+}
