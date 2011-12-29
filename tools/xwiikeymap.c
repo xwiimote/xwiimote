88d9f2a9c5282853f0171adba183f747f78ffc2a
David Herrmann <dh.herrmann@googlemail.com>
2011-12-29 22:55:26 +0100

    Add xwiikeymap dummy
    They xwiikeymap will become the most basic emulator which simply remaps the Wii
    Remote keys to other keys.
    
    Signed-off-by: David Herrmann <dh.herrmann@googlemail.com>

---
+++ tools/xwiikeymap.c
@@ -0,0 +1,20 @@
+/*
+ * XWiimote - tools - xwiikeymap
+ * Written 2011 by David Herrmann
+ * Dedicated to the Public Domain
+ */
+
+/*
+ * XWiimote Button Remapper
+ * This tool listens for incoming Wii Remotes and for every connected Wii Remote
+ * it creates a new input device via uinput and maps the keys as given in the
+ * keymap. This allows to map the buttons of the Wii Remote to arbitrary keys.
+ */
+
+#include <stdlib.h>
+#include "xwiimote.h"
+
+int main(int argc, char **argv)
+{
+	return EXIT_FAILURE;
+}
